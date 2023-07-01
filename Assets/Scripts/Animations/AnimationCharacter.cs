6beaa8325b00f70c192a05f2ebe2925f93e561c7
Emir Han Mamak <emirhanmamak6@gmail.com>
2023-07-01 23:13:59 +0300

    Character Animations Added, Attack Animation Created

---
+++ Assets/Scripts/Animations/AnimationCharacter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Hit()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
