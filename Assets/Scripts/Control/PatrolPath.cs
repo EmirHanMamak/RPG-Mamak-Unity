a9fb9893e3061e751145ff5cf5c30286c689a093
Emir Han Mamak <emirhanmamak6@gmail.com>
2023-07-10 12:34:14 +0300

    Patrol Path added with WayPoint and added to Gizmos

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float wayPointGizmosRadius = 0.4f;
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWayPoint(i), wayPointGizmosRadius);
+                Gizmos.DrawLine(GetWayPoint(i), GetWayPoint(j));
+            }
+
+        }
+
+        private Vector3 GetWayPoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+        private int GetNextIndex(int i)
+        {
+            if (i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+    }
+
+}
