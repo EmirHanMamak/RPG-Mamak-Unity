011ba5307d9f8c069207b15d55a60a7773bb3409
Emir Han Mamak <emirhanmamak6@gmail.com>
2023-07-14 17:21:11 +0300

    Poratl load system improved

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,12 +1,22 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Portal : MonoBehaviour {
+public class Portal : MonoBehaviour
+{
     [SerializeField] int loadSceneIndex = -1;
-    private void OnTriggerEnter(Collider other) {
-        if(other.tag == "Player")
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
         {
-            SceneManager.LoadScene(loadSceneIndex);
+            StartCoroutine(Transition());
         }
+    }
+    private IEnumerator Transition()
+    {
+        DontDestroyOnLoad(gameObject);
+        yield return SceneManager.LoadSceneAsync(loadSceneIndex);
+        print("Scence loaded");
+        Destroy(gameObject);
     }
 }
