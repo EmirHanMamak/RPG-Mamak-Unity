df7ee9214e9555f4a8187b334227796bfe7cddf7
Emir Han Mamak <emirhanmamak6@gmail.com>
2023-07-14 18:20:55 +0300

    SpawnPoint Added

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -5,18 +5,41 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour
 {
     [SerializeField] int loadSceneIndex = -1;
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            StartCoroutine(Transition());
+    [SerializeField] Transform spawnPoint;
+    private void OnTriggerEnter(Collider other) {
+            if (other.tag == "Player")
+            {
+                StartCoroutine(Transition());
+            }
         }
-    }
-    private IEnumerator Transition()
-    {
-        DontDestroyOnLoad(gameObject);
-        yield return SceneManager.LoadSceneAsync(loadSceneIndex);
-        print("Scence loaded");
-        Destroy(gameObject);
-    }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(loadSceneIndex);
+            
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal.spawnPoint.position;
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+
+            return null;
+        }
 }
