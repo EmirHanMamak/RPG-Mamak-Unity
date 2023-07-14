a5009d833f3ddd83f76c1a51de3954e57e8fff98
Emir Han Mamak <emirhanmamak6@gmail.com>
2023-07-14 15:56:40 +0300

    Portal prefabs and codes work now.

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour {
+    [SerializeField] int loadSceneIndex = -1;
     private void OnTriggerEnter(Collider other) {
-        //print("portal triigerd");
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene(loadSceneIndex);
+        }
     }
 }
