c6ec679800daa4ca67f58500b783c38dd6d5fad3
Emir Han Mamak <emirhanmamak6@gmail.com>
2023-07-15 10:32:26 +0300

    Portal Are creacted And work now

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,45 +1,51 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 public class Portal : MonoBehaviour
 {
-    [SerializeField] int loadSceneIndex = -1;
+    enum DestinationIdentifier
+    {
+		A, B, C, D, E
+    }
+    [SerializeField] int sceneToLoad = -1;
     [SerializeField] Transform spawnPoint;
-    private void OnTriggerEnter(Collider other) {
-            if (other.tag == "Player")
-            {
-                StartCoroutine(Transition());
-            }
-        }
-
-        private IEnumerator Transition()
+	[SerializeField] DestinationIdentifier destinationIdentifier;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
         {
-            DontDestroyOnLoad(gameObject);
-            yield return SceneManager.LoadSceneAsync(loadSceneIndex);
-            
-            Portal otherPortal = GetOtherPortal();
-            UpdatePlayer(otherPortal);
-
-            Destroy(gameObject);
+            StartCoroutine(Transition());
         }
+    }
 
-        private void UpdatePlayer(Portal otherPortal)
+    private IEnumerator Transition()
+    {
+        DontDestroyOnLoad(gameObject);
+        yield return SceneManager.LoadSceneAsync(sceneToLoad);
+        Portal otherPortal = GetOtherPortal();
+        UpdatePlayer(otherPortal);
+        Destroy(gameObject);
+    }
+
+    private void UpdatePlayer(Portal otherPortal)
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        print("logg");
+        player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+        player.transform.rotation = otherPortal.spawnPoint.rotation;
+    }
+
+    private Portal GetOtherPortal()
+    {
+        foreach (Portal portal in FindObjectsOfType<Portal>())
         {
-            GameObject player = GameObject.FindWithTag("Player");
-            player.transform.position = otherPortal.spawnPoint.position;
-            player.transform.rotation = otherPortal.spawnPoint.rotation;
+            if (portal == this) continue;
+			if (portal.destinationIdentifier != destinationIdentifier) continue;
+            return portal;
         }
 
-        private Portal GetOtherPortal()
-        {
-            foreach (Portal portal in FindObjectsOfType<Portal>())
-            {
-                if (portal == this) continue;
-
-                return portal;
-            }
-
-            return null;
-        }
+        return null;
+    }
 }
