13f0f6054ddec26c6eac34fe619d3b3ed863523c
Emir Han Mamak <emirhanmamak6@gmail.com>
2023-07-17 09:49:08 +0300

    Sword PickUp done, Fİghter class updated, WeaponPickUp class created.

---
+++ Assets/Scripts/Combat/WeaponPickUp.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class WeaponPickUp : MonoBehaviour
+    {
+        [SerializeField] Weapon weapon;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                Debug.Log("Girdi");
+                other.GetComponent<Fighter>().EquipWeapon(weapon);
+                Destroy(gameObject);
+            }
+
+        }
+    }
+}
