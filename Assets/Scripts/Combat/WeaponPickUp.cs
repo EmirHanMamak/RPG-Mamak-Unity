5eeff48def488e50d5c2a5b29baac76c0365c82d
Emir Han Mamak <emirhanmamak6@gmail.com>
2023-08-26 11:36:01 +0300

    Cursor Type With IRaycastable.

--- Assets/Scripts/Combat/WeaponPickUp.cs
+++ Assets/Scripts/Combat/WeaponPickUp.cs
@@ -70,5 +70,10 @@ namespace RPG.Combat
             }
             return true;
         }
+
+        public CursorType GetCursorType() 
+        {
+           return CursorType.Pickup;
+        }
     }
 }
